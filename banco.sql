398786724aff03297ed7a49ce8cc7bba3fbb24d9
pcpinhal <pc_pinhal@yahoo.com.br>
2024-12-17 11:55:49 -0300

    criando banco fase 1

---
+++ banco.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS login;
+CREATE DATABASE IF NOT EXISTS login;
+USE login;
+
+CREATE TABLE IF NOT EXISTS tb_login (
+id_login				INT NOT NULL AUTO_INCREMENT,
+usuario				VARCHAR(255) NOT NULL,
+senha					VARCHAR(255) NOT NULL,
+nivel					CHAR(1) NOT NULL,
+PRIMARY KEY(id_login)
+);
+SELECT * FROM tb_login;
